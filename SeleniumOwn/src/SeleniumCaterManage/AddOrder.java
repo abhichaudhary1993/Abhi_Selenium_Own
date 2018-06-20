e29145f21e6b01695d0580ea48aa182fda9969ff
Abhi <Abhi@DESKTOP-7H5726N>
2018-06-20 16:31:30 +0530

    catermanage Add order

--- SeleniumOwn/src/SeleniumCaterManage/AddOrder.java
+++ SeleniumOwn/src/SeleniumCaterManage/AddOrder.java
@@ -1,9 +1,13 @@
 package SeleniumCaterManage;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 public class AddOrder {
@@ -18,57 +22,52 @@ public class AddOrder {
 				//Url Invoke
 				
 				driver.get("http://www.catermanage.co.uk/");
-				Thread.sleep(1000);
+				
 				driver.manage().window().maximize();
+				
+				driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 				
 				//Enter Credentials
 				
 				driver.findElement(By.className("md-input")).sendKeys("cater@gmail.com",Keys.TAB,"admin@123",Keys.ENTER);
 				
 				
-				Thread.sleep(5000);
+				
 				//Locate Order
 				driver.findElement(By.xpath("//span[text()='Order']")).click();
 				
+				
+				
 				Thread.sleep(2000);
-				
-				
 				
 				
 		
 				
 				//Click on Add order
-				driver.findElement(By.linkText("Add Order")).click();
-				Thread.sleep(1000);
+				 driver.findElement(By.xpath("//*[@id='menu_top']/li[3]/a/span")).click();
+				 Actions a = new Actions(driver);
+				 WebElement e = driver.findElement(By.linkText("Add Order"));
+				 a.moveToElement(e).click().build().perform();
+				/* //Move t the manage order
+				 WebElement e1 = driver.findElement(By.linkText("Manage Orders"));
+				 a.moveToElement(e1).click().build().perform();*/
+				 
+			
+			
 				
 //////////////////////////////////////////////Step 1///////////////////////////////////////////////////////////////////////////////////////////////////	
 				
-				
-				//click on order date
-				driver.findElement(By.xpath("//*[@id='datepicker1']")).click();
-				Thread.sleep(1000);
-				driver.findElement(By.id("datepicker1")).click();
-				
-				
-				/*driver.findElement(By.name("note")).sendKeys("Hello I want to book order for 20 Guests . Thank You ");
-				Thread.sleep(1000);
-				
-				//select customer
-				Select stp1a = new Select(driver.findElement(By.id("select_demo_5")));
-				stp1a.selectByValue("93");
-				Thread.sleep(3000);
-				//Select event venue and give address
-				driver.findElement(By.name("event_venue")).sendKeys("A 20, laxman puri ,vipin garden , near dwarka mor metro station , new delhi 110059 ");
-				Thread.sleep(2000);
-				
-				//select time 
-				driver.findElement(By.xpath("//input[@id = 'uk_tp_1']")).click();
-				Thread.sleep(1000);
-				//select no of guest
-				driver.findElement(By.name("num_guest")).sendKeys("20");
-				Thread.sleep(1000);*/
-				
-				
+				 driver.findElement(By.xpath("(//input[@type='text'])[3]")).click();
+				 driver.findElement(By.xpath("//*[@class='day']")).click();
+				 driver.findElement(By.xpath("(//input[@type='text'])[4]")).click();
+				 driver.findElement(By.xpath("//*[@class='day']")).click();
+				 driver.findElement(By.name("note")).sendKeys("Sir i want to order some things on your website for party");
+				 
+				 
+				Select customer_name = new Select(driver.findElement(By.name("seller_company_name")));
+				customer_name.selectByValue("93");
+				 
+			
 				
 				
 				
